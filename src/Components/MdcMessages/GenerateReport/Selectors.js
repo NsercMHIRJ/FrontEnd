b11e5317328907d142d2371c226fee57cb9f3d40
Juliana Claussen <juliana.correa.couto@gmail.com>
2022-01-09 18:00:47 -0500

    Match latest changes from new repo

--- src/Components/MdcMessages/GenerateReport/Selectors.js
+++ src/Components/MdcMessages/GenerateReport/Selectors.js
@@ -140,7 +140,9 @@ export const EqIDSelector = (props) => {
         var data = JSON.parse(res.data);
         let EQArray = ['NONE'];
         Object.values(data).map((item=>{
-          EQArray.push(item.EQ_ID.toString());
+          if (item.EQ_ID) {
+            EQArray.push(item.EQ_ID.toString());
+          }
         }))
         setEqIDList(EQArray);
       });
